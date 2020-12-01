805b744feb49fb1b045d6a51133865a2e778a132
Juan <juanpablodrmatos@gmail.com>
2020-12-01 10:12:07 -0300

    website checking added

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
 module Lib
-    ( someFunc
+    ( 
+        checaEstoque
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Network.HTTP.Conduit
+import Text.HTML.TagSoup
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy.Char8 as CL
+
+
+checaEstoque :: IO ()
+checaEstoque = do
+    lbs <- simpleHttp "https://en.wikipedia.org/wiki/Main_Page"
+    let lastModifiedDateTime = fromFooter $ parseTags lbs
+    putStrLn $ "wikipedia was last modified on " 
+        ++ CL.unpack lastModifiedDateTime
+    where fromFooter = CL.unwords . drop 6 . CL.words
+              . innerText . take 2 . dropWhile (~/= "<li id=footer-info-lastmod>")
