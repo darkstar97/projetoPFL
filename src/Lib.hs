ec474e2ce1cf9664d353ace7839fdb53d31a6423
Juan <juanpablodrmatos@gmail.com>
2020-12-04 19:34:52 -0300

    2048

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,17 +3,6 @@ module Lib
         checaEstoque
     ) where
 
-import Network.HTTP.Conduit
-import Text.HTML.TagSoup
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Lazy.Char8 as CL
-
-
 checaEstoque :: IO ()
 checaEstoque = do
-    lbs <- simpleHttp "https://en.wikipedia.org/wiki/Main_Page"
-    let lastModifiedDateTime = fromFooter $ parseTags lbs
-    putStrLn $ "wikipedia was last modified on " 
-        ++ CL.unpack lastModifiedDateTime
-    where fromFooter = CL.unwords . drop 6 . CL.words
-              . innerText . take 2 . dropWhile (~/= "<li id=footer-info-lastmod>")
+    putStrLn "ayayaya"
